c8e105964c1e24aef4ce93540f21758253c9258a
oumaimalharrak <oumaimalharrak96@gmail>
2022-01-28 00:29:35 +0100

    First commit

---
+++ Desktop/pyApp/app.py
@@ -0,0 +1,33 @@
+
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_track_modification'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete= db.Column(db.Boolean)
+
+
+@app.route("/")
+def index():
+    #show all todo
+    todo_list= Todo.query.all()
+    print(todo_list)
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    db.create_all()
+    new_todo=Todo(title="todo 1", complete=False)
+    db.session.add(new_todo)
+    db.session.commit
+
+
+    app.run(debug=True)
+
